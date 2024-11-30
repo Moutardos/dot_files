59b256b77a8e0ac6b9b6eed3e66931924a11f9cb
lcozden <lcozdenm@student.42.fr>
2024-11-30 17:33:08 +0100

    config.fish : typo for env path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,9 @@
-
 ## virtual env
 export WORKON_HOME=$HOME/.virtualenvs
 # check python exec path
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
-export VIRTUALENVWRAPPER_VIRTUALfish_add_path $HOME/.local/bin
-set ENV_ARGS ' -p /usr/bin/python3 '
+# set VIRTUALENVWRAPPER_VIRTUALENV_ARGS ' -p /usr/bin/python3 '
+export VIRTUALENVWRAPPER_VIRTUALENV_ARGS=' -p /usr/bin/python3 '
 export PROJECT_HOME=$HOME/Documents/42
 source $HOME/.local/bin/fishenv.fish
 
