1010e7377a2ebc7c5751c947240f7247fbd6a9d1
lcozdenm <moutarda@proton.me>
2025-03-24 16:14:54 +0100

    nvim: colorscheme is now cyberdream, vim options on it's own file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,7 @@
--- Vim stuff
-vim.opt.nu = true
-vim.opt.rnu = true
-vim.opt.autoindent = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.textwidth = 0
-vim.opt.shiftwidth = 4
+require("config.options")
+
 -- Lazy stuff
 require("config.lazy")
+-- Colorscheme
+vim.cmd.colorscheme("cyberdream")
+vim.api.nvim_set_keymap("n", "<leader>tt", ":CyberdreamToggleMode<CR>", { noremap = true, silent = true })
---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,17 @@
+-- Vim stuff
+vim.opt.nu = true
+vim.opt.rnu = true
+
+vim.opt.autoindent = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.textwidth = 0
+vim.opt.shiftwidth = 4
+
+vim.opt.virtualedit = "block"
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true
+vim.opt.termguicolors = true
+vim.opt.smartindent = true
+
+vim.g.mapleader = " "
