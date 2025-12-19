f0fb1b4449e4a5c2bd0e082986aa4f1d04204f41
lcozdenm <lcozdenm@student.42.fr>
2025-12-19 19:09:14 +0100

    Changed deprecated  lspconfig[x] into vim.lsp.config.x Added keymap on own file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,8 @@
 require("config.options")
-
 -- Lazy stuff
 require("config.lazy")
-require('mini.surround').setup()
-
+require("mini.surround").setup()
+require("config.lsp-keymap")
 -- Colorscheme
 vim.cmd.colorscheme("cyberdream")
 vim.api.nvim_set_keymap("n", "<leader>tt", ":CyberdreamToggleMode<CR>", { noremap = true, silent = true })
--- .config/nvim/lua/plugins/lsp/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp/lsp-config.lua
@@ -1,31 +1,40 @@
-return
-{
+return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
-		dependencies = { "hrsh7th/nvim-cmp" },
 	},
 	config = function()
-		local lspconfig = require("lspconfig")
 		local cmp_lsp = require("cmp_nvim_lsp")
-		local capabilities = cmp_lsp.default_capabilities()
-		lspconfig["clangd"].setup({
-			
-		})
-        lspconfig["lua_ls"].setup({
-          capabilities = capabilities,
-          settings = {
-            Lua = {
-              -- make the language server recognize "vim" global
-              diagnostics = {
-                globals = { "vim" },
-			  },
-              completion = {
-                callSnippet = "Replace",
-				}
-			}
+		-- local capabilities = cmp_lsp.default_capabilities()
+		local on_attach = function(client, bufnr)
+			local opts = { noremap = true, silent = true, buffer = bufnr }
+			local keymap = vim.keymap.set
+			-- Hover and go to definition
+			keymap("n", "K", vim.lsp.buf.hover, opts)
+			keymap("n", "gd", vim.lsp.buf.definition, opts)
+		end
+
+		vim.lsp.config.lua_ls = {
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+					workspace = {
+						library = {
+							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+							[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+						},
+						maxPreload = 100000,
+						preloadFileSize = 10000,
+					},
+				},
+			},
 		}
-	  })
-	end
+		-- Enable the LSP servers
+		vim.lsp.enable("clangd")
+		vim.lsp.enable("lua_ls")
+		-- For rust, see rustaceanvim.lua
+	end,
 }
