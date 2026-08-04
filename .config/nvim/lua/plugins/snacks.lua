2ae6855a4e1e4e94f3ebb788444e8d7bdbbac2f0
loic.cozdenmat <loic.cozdenmat@kelenntech.com>
2026-08-04 10:25:39 +0200

    fixed nvim/nvim non used config

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -3,39 +3,42 @@ return {
 	priority = 1000,
 	lazy = false,
 	---@type snacks.Config
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-		bigfile = { enabled = true },
-		dashboard = { enabled = true },
-		explorer = { enabled = true },
-		indent = { enabled = true },
-		input = { enabled = true },
-		notifier = { enabled = true },
-		quickfile = { enabled = true },
-		scope = { enabled = true },
-		scroll = { enabled = true },
-		statuscolumn = { enabled = true },
-		words = { enabled = true },
-		picker = {
-			enabled = true,
-			layout = {
-				preset = "vertical",
-			},
-			formatters = {
-				file = {
-					filename_first = true,
-					truncate = 80,
+	opts = function()
+		local dashboard_conf = require("config.dashboard")
+		return {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+			dashboard = dashboard_conf,
+			bigfile = { enabled = true },
+			explorer = { enabled = true },
+			indent = { enabled = true },
+			input = { enabled = true },
+			notifier = { enabled = true },
+			quickfile = { enabled = true },
+			scope = { enabled = true },
+			scroll = { enabled = true },
+			statuscolumn = { enabled = true },
+			words = { enabled = true },
+			picker = {
+				enabled = true,
+				layout = {
+					preset = "vertical",
 				},
-			},
-			win = {
-				input = {
-					keys = {
-						["<Esc>"] = "close",
+				formatters = {
+					file = {
+						filename_first = true,
+						truncate = 80,
+					},
+				},
+				win = {
+					input = {
+						keys = {
+							["<Esc>"] = "close",
+						},
 					},
 				},
 			},
-		},
-	},
+		}
+	end,
 }
